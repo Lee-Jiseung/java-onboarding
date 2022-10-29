14e4c013fd03954b28f21332ea142e790e569436
Lee-Jiseung <ljss98@naver.com>
2022-10-29 21:36:33 +0900

    feat: 숫자를 배열로 바꾸는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,15 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static int[] int2Array(Integer pageNumber) {
+        return Stream.of(String.valueOf(pageNumber.intValue()).split("")).mapToInt(Integer::parseInt).toArray();
     }
 }
